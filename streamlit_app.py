c44705a2789b0fa41552d13e13264b4eb4033dd8
toto <supertoto.a@gmail.com>
2025-03-06 09:16:22 +0100

    vnenwo with vne first 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ from utils.st_login import check_password
 
 #database is on https://cloud.tembo.io/
 
-CLAN_NAME = "NTV"
+CLAN_NAME = "VNENWO"
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
