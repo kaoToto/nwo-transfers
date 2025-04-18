5a0fb1b332aa92c32201a32cfee3d21d2fd3a9b1
toto <supertoto.a@gmail.com>
2025-04-18 10:48:32 +0200

    fix clan names

--- streamlit_app.py
+++ streamlit_app.py
@@ -694,8 +694,8 @@ if "players_ranks_df" in st.session_state:
 
     st.session_state.players_ranks_df['player_id'] = st.session_state.players_ranks_df['ID']
     merged_df = st.session_state.players_ranks_df
-    st.dataframe(merged_df)
     merged_df['clan_name']= merged_df['Current Clan Name']
+    st.dataframe(merged_df)
 
 #    merged_df = generals_df.merge(st.session_state.players_ranks_df[['player_id', 'Name', 'Current Clan']], 
 #                             on= 'player_id', 
